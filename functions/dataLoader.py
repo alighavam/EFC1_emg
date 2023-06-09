107f7e8ef4ec686fd2dc054e30c157e38cf70bd8
Ali Ghavampour <alighavam79@gmail.com>
2023-06-09 19:01:24 -0400

    started emgLoader section

--- functions/dataLoader.py
+++ functions/dataLoader.py
@@ -1,7 +1,9 @@
 import numpy as np
+import pandas as pd
+import emgHandler
 
-# loads .mov files given the path of the file. The .mov files have a specific custom hence the need for a custom function.
 def movload(fname):
+    # loads .mov files given the path of the file. The .mov files have a specific custom hence the need for a custom function
     A = []
     fid = open(fname, 'rt')
     if fid == -1:
@@ -29,3 +31,19 @@ def movload(fname):
     return A
 
 
+def emgload(fname): # handles loading emg files
+
+    # loading emg file:
+    column_names = [i for i in range(0, 7)]
+    data = pd.read_csv(fname, header=None, delimiter=',', names=column_names)   # loads .csv file into a dataframe
+    fs = float(data[0][6].split()[0])   # getting the sampling rate of the data
+    raw_emg = pd.DataFrame(data.iloc[7:], dtype=float)  # making a new dataframe containing only the raw emg data
+    raw_emg = raw_emg.reset_index(drop=True)
+    
+    # finding triggers:
+    trig = raw_emg[0]   # trigger channel
+    emgHandler.find_trigger_rise_edge(trig, fs, riseThresh=0.5, fallThresh=0.5, debug=1)
+    
+
+
+emgload('/Users/aghavampour/Desktop/Projects/ExtFlexChord/EFC1_emg/data/subj99/efc1_EMG_99_01.csv')
