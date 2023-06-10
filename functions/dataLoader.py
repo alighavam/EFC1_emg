62f9ffc426fbce4085c7c9d9f6d201db389bc1b8
Ali Ghavampour <alighavam79@gmail.com>
2023-06-10 19:43:12 -0400

    added comments to emgload() + completed the code

--- functions/dataLoader.py
+++ functions/dataLoader.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import emgHandler
+from functions import emgHandler
 
 def movload(fname):
     # loads .mov files given the path of the file. The .mov files have a specific custom hence the need for a custom function
@@ -31,19 +31,50 @@ def movload(fname):
     return A
 
 
-def emgload(fname): # handles loading emg files
+def emgload(fname, riseThresh=0.5, fallThresh=0.5, debug=0): # handles loading emg files
+    '''
+        Description: Loads and handles the emg data from fname.csv file. 
+
+        <inputs>
+        fname: the .csv file name
+
+        riseThresh, fallThresh, debug: refer to emgHandler.find_trigger_rise_edge function.
+
+        <outputs>
+        emg_selected: python list with len <number of trials>. Each list element is emg data for each tiral. It is a numpy array with the format of (N by ChannelNum). 
+        N is the len of that trial. ChannelNum is the number of emg channels.
+
+        fs: sampling frequency of the emg data.
+    '''
 
     # loading emg file:
-    column_names = [i for i in range(0, 7)]
+    column_names = [i for i in range(0, 7)] # hard coded number of columns in dataframe - ideally you should calculate maximum of number of columns for future use of the code
     data = pd.read_csv(fname, header=None, delimiter=',', names=column_names)   # loads .csv file into a dataframe
-    fs = float(data[0][6].split()[0])   # getting the sampling rate of the data
-    raw_emg = pd.DataFrame(data.iloc[7:], dtype=float)  # making a new dataframe containing only the raw emg data
+    fs = float(data[0][6].split()[0])   # getting the sampling rate of the data - hard coded to get fs. Also, assumed that all emg channels have the same fs.
+    raw_emg = pd.DataFrame(data.iloc[7:], dtype=float)  # making a new dataframe containing only the raw emg data - hard coded row num
     raw_emg = raw_emg.reset_index(drop=True)
     
     # finding triggers:
     trig = raw_emg[0]   # trigger channel
-    emgHandler.find_trigger_rise_edge(trig, fs, riseThresh=0.5, fallThresh=0.5, debug=1)
+    riseIdx, fallIdx = emgHandler.find_trigger_rise_edge(trig, fs, riseThresh, fallThresh, debug) # trigger detector function
     
+    # slecting emg data of each trial based on triggers
+    emg_selected = [] # empty list to contain emg data
+    for i in range(len(riseIdx)):
+        # starting index of trial i:
+        idxStart = riseIdx[i]
+
+        # ending index of trial i:
+        idxEnd = fallIdx[i]     
+
+        # selecting emg data. Channel 0 is trigger so it's not included:
+        emg_tmp = raw_emg.iloc[idxStart:idxEnd,1:].to_numpy()  
+
+        # append trial's emg data to emg_selected , the format of emg_tmp is (N by ChannelNum):
+        emg_selected.append(emg_tmp)   
+        # print("Trial {}:\n".format(i), len(emg_tmp[:,0])) # a sanity check
+    
+    # print(np.shape(emg_selected[99])) # sanity check
+    return emg_selected, fs
 
 
-emgload('/Users/aghavampour/Desktop/Projects/ExtFlexChord/EFC1_emg/data/subj99/efc1_EMG_99_01.csv')
